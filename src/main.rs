04f03a29106411d766f775fc0a7b8a52d9e492e7
Ben Brandt <benjamin.j.brandt@gmail.com>
2019-04-22 21:15:16 -0700

    Add sentry logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use actix_web::middleware::cors::Cors;
 use actix_web::{http, server, App, HttpResponse, Query, Responder};
+use sentry;
+use sentry_actix::SentryMiddleware;
 use serde::Deserialize;
 use std::env;
 
@@ -22,6 +24,11 @@ fn index(query: Query<RollQuery>) -> impl Responder {
 }
 
 fn main() {
+    let _guard = sentry::init("https://046b94f8170f4135a47ca9d0f9709a6d@sentry.io/1438468");
+    env::set_var("RUST_BACKTRACE", "1");
+    sentry::integrations::env_logger::init(None, Default::default());
+    sentry::integrations::panic::register_panic_handler();
+
     // Get the port number to listen on.
     let port = env::var("PORT")
         .unwrap_or_else(|_| "3000".to_string())
@@ -30,11 +37,13 @@ fn main() {
 
     // Start a server, configuring the resources to serve.
     server::new(|| {
-        App::new().configure(|app| {
-            Cors::for_app(app)
-                .resource("/", |r| r.method(http::Method::GET).with(index))
-                .register()
-        })
+        App::new()
+            .configure(|app| {
+                Cors::for_app(app)
+                    .resource("/", |r| r.method(http::Method::GET).with(index))
+                    .register()
+            })
+            .middleware(SentryMiddleware::new())
     })
     .bind(("0.0.0.0", port))
     .unwrap_or_else(|_| panic!("Can not bind to port {:?}", &port))

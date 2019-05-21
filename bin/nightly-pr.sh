29cf568c9a15a6702e9b4e591290b1127077ab28
Ben Brandt <benjamin.j.brandt@gmail.com>
2019-05-21 14:36:17 -0700

    Create nightly-pr.sh

---
+++ bin/nightly-pr.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install Hub
+HUB_DIR=hub-linux-amd64-2.11.2
+curl -sL "https://github.com/github/hub/releases/download/v2.11.2/$HUB_DIR.tgz" | tar xz
+
+# YYYY-mm-dd format
+CURRENT_DATE=`date +"%F"`
+TOOLCHAIN="nightly-$CURRENT_DATE"
+MESSAGE="Update toolchain to $TOOLCHAIN"
+
+# Checkout a branch for today
+git checkout -b rust-$TOOLCHAIN
+
+# Save the date to the toolchain file
+echo $TOOLCHAIN > 'rust-toolchain'
+
+# Stage and commit the change
+git add 'rust-toolchain'
+git commit -m "$MESSAGE"
+
+# Create pull request
+./$HUB_DIR/bin/hub pull-request -m "$MESSAGE" --push --reviewer benbrandt --labels rust,toolchain
+
+# Cleanup
+git checkout master
+rm -rf $HUB_DIR

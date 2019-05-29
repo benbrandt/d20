548535f6e4e9cdc3530d3854639e4b9adfa206c1
Ben Brandt <benjamin.j.brandt@gmail.com>
2019-05-28 22:14:23 -0700

    Simplify some things

--- bin/nightly-pr.sh
+++ bin/nightly-pr.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Setup config
-mkdir -p ~/.config
-
-echo "github.com:
-- user: $GITHUB_USER
-  oauth_token: $GITHUB_TOKEN
-  protocol: https" >> ~/.config/hub
-
 # Install Hub
 HUB_DIR=hub-linux-amd64-2.11.2
 curl -sL "https://github.com/github/hub/releases/download/v2.11.2/$HUB_DIR.tgz" | tar xz
@@ -15,18 +7,19 @@ curl -sL "https://github.com/github/hub/releases/download/v2.11.2/$HUB_DIR.tgz" 
 # YYYY-mm-dd format
 CURRENT_DATE=`date +"%F"`
 TOOLCHAIN="nightly-$CURRENT_DATE"
+BRANCH="rust/$TOOLCHAIN"
 MESSAGE="Update toolchain to $TOOLCHAIN"
 
 # Checkout a branch for today
-./$HUB_DIR/bin/hub checkout -b rust-$TOOLCHAIN
+git checkout -b BRANCH
 
 # Save the date to the toolchain file
 echo $TOOLCHAIN > 'rust-toolchain'
 
 # Stage and commit the change
-./$HUB_DIR/bin/hub add 'rust-toolchain'
-./$HUB_DIR/bin/hub commit -m "$MESSAGE"
-./$HUB_DIR/bin/hub push --set-upstream origin rust-$TOOLCHAIN
+git add 'rust-toolchain'
+git commit -m "$MESSAGE"
+git push --set-upstream origin BRANCH
 
 # Create pull request
-./$HUB_DIR/bin/hub pull-request -m "$MESSAGE" --assign benbrandt
+./$HUB_DIR/bin/hub pull-request -m "$MESSAGE" --assign benbrandt --labels rust

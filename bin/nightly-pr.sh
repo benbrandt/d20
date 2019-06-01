e64cfc49fb51761333637ef355062c891f974c54
Ben Brandt <benjamin.j.brandt@gmail.com>
2019-05-31 19:56:45 -0700

    Fix branch name

--- bin/nightly-pr.sh
+++ bin/nightly-pr.sh
@@ -11,7 +11,7 @@ BRANCH="rust/$TOOLCHAIN"
 MESSAGE="Update toolchain to $TOOLCHAIN"
 
 # Checkout a branch for today
-git checkout -b BRANCH
+git checkout -b $BRANCH
 
 # Save the date to the toolchain file
 echo $TOOLCHAIN > 'rust-toolchain'
@@ -19,7 +19,7 @@ echo $TOOLCHAIN > 'rust-toolchain'
 # Stage and commit the change
 git add 'rust-toolchain'
 git commit -m "$MESSAGE"
-git push --set-upstream origin BRANCH
+git push --set-upstream origin $BRANCH
 
 # Create pull request
 ./$HUB_DIR/bin/hub pull-request -m "$MESSAGE" --assign benbrandt --labels rust

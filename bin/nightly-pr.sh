21dc3209ca46997704d4f52a42db5b404437ee76
Ben Brandt <benjamin.j.brandt@gmail.com>
2019-08-08 09:02:12 -0700

    Update nightly-pr.sh

--- bin/nightly-pr.sh
+++ bin/nightly-pr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install Hub
-HUB_DIR=hub-linux-amd64-2.11.2
-curl -sL "https://github.com/github/hub/releases/download/v2.11.2/$HUB_DIR.tgz" | tar xz
+HUB_DIR=hub-linux-amd64-2.12.3
+curl -sL "https://github.com/github/hub/releases/download/v2.12.3/$HUB_DIR.tgz" | tar xz
 
 # YYYY-mm-dd format
 CURRENT_DATE=`date +"%F"`
